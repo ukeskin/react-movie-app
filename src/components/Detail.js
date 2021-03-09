a33d5fc753721ce51ae01f2dfca153ed46a3d2f9
ukeskin <ukeskin2626@gmail.com>
2021-03-09 18:03:02 +0300

    details page update

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -20,13 +20,15 @@ function Detail() {
           className="h-96 w-80"
           src={"https://image.tmdb.org/t/p/w500" + details.poster_path}
         />
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-evenly  p-4 border-r-4 ">
           <h1 className="text-2xl text-white font-bold">
-            {details.title} -{" "}
-            <span className="font-thin">{details.release_date}</span>
+            {details.title} -
+            <span className="text-xl text-yellow-400 font-bold">
+              ⭐{details.vote_average}
+            </span>
           </h1>
-
-          <p className="text-2xl text-white">{details.overview}</p>
+          <q className="text-white text-xl">{details.tagline}</q>
+          <span className="text-2xl text-white">{details.overview}</span>
         </div>
       </div>
     </div>
